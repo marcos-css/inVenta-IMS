28f8b2a943a96151d3e4c3414dfcfd3a4a52cef8
Marcos <marcos.csca@gmail.com>
2026-06-23 21:17:46 -0600

    fix: se corrigío la propiedad marca a 'marca?' en la interfaz 'Producto' en caso que la base de datos devuelva un valor nulo

--- inVentaApp/src/types/index.ts
+++ inVentaApp/src/types/index.ts
@@ -1,21 +1,20 @@
-export interface Categoria{
-    id: string;
-    nombre: string;
-    estadoActivo: boolean;
-    sincronizado: boolean;
-    fechaActualizacion: string;
+export interface Categoria {
+  id: string;
+  nombre: string;
+  estadoActivo: boolean;
+  sincronizado: boolean;
+  fechaActualizacion: string;
 }
 
-export interface Producto{
-    id: string;
-    nombre: string;
-    marca: string;
-    precio: number;
-    categoriaId: string;
-    estadoActivo: boolean;
-    sincronizado: boolean;
-    fechaActualizacion: string;
-    //propiedad de navegacion opcional para mostrar el nombre de la categoria en lugar del id
-    categoria?: Categoria;
-
+export interface Producto {
+  id: string;
+  nombre: string;
+  marca?: string;
+  precio: number;
+  categoriaId: string;
+  estadoActivo: boolean;
+  sincronizado: boolean;
+  fechaActualizacion: string;
+  //propiedad de navegacion opcional para mostrar el nombre de la categoria en lugar del id
+  categoria?: Categoria;
 }
